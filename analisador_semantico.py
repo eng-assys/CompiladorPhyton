dbce6e4c5306db0520946d6111c8f0f4ee2306dd
V_assys <v_assys@localhost.localdomain>
2015-10-01 23:54:56 -0300

    Iniciando criação de tabela de símbolos do analisador semântico

--- analisador_semantico.py
+++ analisador_semantico.py
@@ -1,1 +1,47 @@
-# Ira conter a instancia do programa que realiza a analise semantica do codigo contido em programa.txt
+# -*- coding: utf-8 -*-
+######           Analisador Semantico    ######
+# Autor 1: Lucas Vinicius dos Santos Assis
+# Autor 2: Andressa Moura de Souza
+# EXA 869 - MI - Processadores de Linguagem de Programacao
+# ==============================================================================
+# Implementacao do analisador semantico de um compilador em python
+
+# Bibliotecas para entrada e saida de arquivos
+import sys
+import os.path
+
+# Bliblioteca padrao de string
+import string
+
+# Declarando Classe do Analisador Semantica
+class AnalisadorSemantico ():
+    def __init__():
+        self.tabela_semantica = {}
+        '''
+            Dentro da tabela de registro devo colocar:
+                * Cada vez que verifiquei a criacao de um novo registro
+                este vira um novo dicionario que deve ser adicionado na
+                tabela de registro
+        '''
+        self.registro_tab = {}
+        '''
+            Guardamos os tipos primitivos e os identificadores
+        '''
+        self.constantes_tab = {}
+        '''
+            Guardamos os tipos primitivos e os identificadores
+        '''
+        self.variaveisGlobais_tab = {}
+        self.funcao_tab = {}
+        self.algoritmo_tab = {}
+
+        '''
+            Construindo estrutura de tabela semantica de simbolos
+        '''
+        self.tabela_semantica["registro"] = self.registro_tab
+        self.tabela_semantica["constantes"] = self.constantes_tab
+        self.tabela_semantica["variaveisGlobais"] = self.variaveisGlobais_tab
+        self.tabela_semantica["funcao"] = self.funcao_tab
+        self.tabela_semantica["algoritmo"] = self.algoritmo_tab
+
+    def analisa():
